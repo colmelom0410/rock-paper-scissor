3f8ab28a576999af843788205a4d1ad1ac3ddd93
Shalom S. De Veyra <191439611+colmelom0410@users.noreply.github.com>
2025-01-03 16:37:57 +0800

    Create a function to get the human choice

--- script.js
+++ script.js
@@ -3,21 +3,41 @@
 // Create a function that will get computer's choice
 function getComputerChoice(){
     // Generate a Random number from 1-3
-    let randomNumber = Math.floor(Math.random()*3) + 1
+    let randomNumber = Math.floor(Math.random()*3) + 1;
+    let computerChoice = "";
     // IF randomNumber == 1 THEN Rock
     if(randomNumber == 1){
-        return console.log("Computer: ROCK")
+        computerChoice = "Rock";
+        return computerChoice;
     }
     // IF randomNumber == 2 THEN Paper
     else if(randomNumber == 2){
-        return console.log("Computer: Paper")
+        computerChoice = "Paper";
+        return computerChoice;
     }
     // IF randomNumber == 3 THEN scissor
     else{
-        return console.log("Computer: Scissor")
+        computerChoice = "Scissor";
+        return computerChoice;
     }
 }
 // Create a function that will get the user's choice
+function getHumanChoice(){
+    // Get the human choice using Prompt
+    let humanChoice = prompt("Rock, Paper, Scissor?: ");
+    // Capitalize the choices to make it easier to compare to computer choice
+    humanChoice = (humanChoice.charAt(0)).toUpperCase() + humanChoice.slice(1).toLowerCase();
+    // IF humanChoice is within the choices(Rock,Paper, Scissor) THEN return humanChoice ELSE try again
+    if (humanChoice == "Rock" || humanChoice == "Paper" || humanChoice == "Scissor"){
+        return humanChoice;
+    }
+    else{
+        return "Rock, Paper, or Scissor ONLY!";
+    }
+        
+}
+
 // create a function that will compare the answer and decide the winner
 
-getComputerChoice()
+console.log(getComputerChoice())
+console.log(getHumanChoice())
